a9637856979240d563ea9ba2ef60e71def6a8806
PATH242 <mennashaheen2357@gmail.com>
2023-03-03 12:34:37 +0400

    minor edits on lexer

---
+++ identifierTable.h
@@ -0,0 +1,11 @@
+//This file could potentially be merged with parser.hpp
+#include <iostream>
+#include <unordered_map>
+#include <stack>
+#define first f
+#define second s
+
+std::unordered_map <std::string, bool> variableNames;
+// pair < variable name, scope number > 
+// -> when a new scope is introduced, its id is (lastScopeId + 1).
+std::stack <std::pair<std::string, int>> variableScope;
--- lexer.h
+++ lexer.h
@@ -10,7 +10,7 @@
 // Replaces `int yylex()` with `symbol_type get_next_token()` to be compatible with bison 3
 #undef YY_DECL
 #define YY_DECL carbonic_c::Parser::symbol_type carbonic_c::Lexer::get_next_token()
-//#include "parser.hpp"
+#include "parser.hpp"
 // Define token types
 enum token_type {
     TK_VAR,
