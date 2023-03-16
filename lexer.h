5d2799bad2ee66b57163c029c2f00d88aafa87b2
IVIosab <mosab.f.r@gmail.com>
2023-03-16 20:19:48 +0300

    [fix][lexer] lexer code was copied twice for some reason

--- lexer.h
+++ lexer.h
@@ -10,99 +10,13 @@
 // Replaces `int yylex()` with `symbol_type get_next_token()` to be compatible with bison 3
 #undef YY_DECL
 #define YY_DECL carbonic_c::Parser::symbol_type carbonic_c::Lexer::get_next_token()
-#include "parser.hpp"
-// Define token types
-enum token_type {
-    TK_VAR,
-    TK_TYPE,
-    TK_INT,
-    TK_DOUBLE,
-    TK_BOOL,
-    TK_CHAR,
-    TK_ARRAY,
-    TK_RECORD,
-    TK_ROUTINE,
-    TK_RETURN,
-    TK_IS,
-    TK_NOT,
-    TK_WHILE,
-    TK_LOOP,
-    TK_END,
-    TK_FOR,
-    TK_FOREACH,
-    TK_FROM,
-    TK_IF,
-    TK_THEN,
-    TK_ELSE,
-    TK_IN,
-    TK_REVERSE,
-    TK_STRING_LITERAL,
-    TK_IDENTIFIER,
-    TK_DOUBLE_LITERAL,
-    TK_INTEGER_LITERAL,
-    TK_CHARACTER_LITERAL,
-    TK_TRUE,
-    TK_FALSE,
-    TK_PRINT,
-    TK_ASSIGN,
-    TK_CEQ,
-    TK_CNEQ,
-    TK_CLT,
-    TK_CLE,
-    TK_CGT,
-    TK_CGE,
-    TK_LPAREN,
-    TK_RPAREN,
-    TK_LBRACK,
-    TK_RBRACK,
-    TK_LSQBRK,
-    TK_RSQBRK,
-    TK_SQUOT,
-    TK_DOT,
-    TK_COMMA,
-    TK_COLON,
-    TK_SCOLON,
-    TK_DDOT,
-    TK_PLUS,
-    TK_MINUS,
-    TK_MUL,
-    TK_DIV,
-    TK_NEWLINE,
-    TK_MOD,
-    TK_POW,
-    TK_AND,
-    TK_OR,
-    TK_XOR
-};
-
-struct variableNames{
-    std::string name;
-    struct variableNames* next;
-};
-typedef struct variableNames variableNames;
-variableNames* variableTable = (variableNames *)0;
-variableNames* addVariable(std::string newId){
-    variableNames* newNode;
-    newNode = (variableNames *) malloc(sizeof(variableNames));
-    newNode->name = newId;
-    newNode->next = (variableNames *) variableTable;
-    variableTable = newNode;
-    return newNode;
-}
-variableNames* getVariable(std::string id){
-    variableNames* currentNode = variableTable;
-    do{
-        if(currentNode->name == id){
-            return currentNode;
-        }
-        currentNode = (variableNames*) currentNode->next;
-    }while(currentNode != (variableNames*)0);
 
 #include "parser.hpp"
 #include <fstream>
 
 // Define token types
-enum token_type {
+enum token_type
+{
     TK_VAR,
     TK_TYPE,
     TK_INT,
