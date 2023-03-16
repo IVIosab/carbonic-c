37b81bbae0e981d101b5d75c1c9b9af8fc4411dc
IVIosab <mosab.f.r@gmail.com>
2023-03-16 19:48:15 +0300

    [tests] update test_parser script

--- test_parser.sh
+++ test_parser.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-for f in ./tests/valid_tests/*.crbc;
+for folder in ./tests/components/*;
 do 
-    name=${f##*/}
-    base=${name%.crbc}
-    echo "Processing $base test..."
-    ./a.out<$f>./tests/parser_outputs/${base}.txt
-    
+    dir_name=${folder##*/}
+    echo "Processing $dir_name tests..."
+    for file in ./tests/components/$dir_name/*.crbc;
+    do
+        file_name=${file##*/} 
+        file_base=${file_name%.crbc}
+        echo -e -n "\tProcessing {$file_base} test... "
+        ./output.out<$f>./tests/parser_outputs/${name}/${base}.txt
+        echo "Completed!"
+    done
+    echo "Completed!"
 done
