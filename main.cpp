761ba2c8243e8146edc01505f97487e7b7385415
Jaffar Totanji <jacepac264@gmail.com>
2023-05-11 19:05:47 +0300

    [delete] unused comments

--- main.cpp
+++ main.cpp
@@ -16,28 +16,13 @@ int main(int argc, char **argv)
     int x = driver.parse_program();
 
     std::cout << std::endl;
-    // // analyzer::AstPrinter printer;
-    // // program->accept(&printer);
+    // analyzer::AstPrinter printer;
+    // program->accept(&printer);
     analyzer::Semantic analyzer;
     program->accept(&analyzer);
     // prettyPrinter::codePrinter printer;
     // program->accept(&printer);
 
-    // // create function signature
-    // llvm::LLVMContext context;
-    // std::unique_ptr<llvm::Module> module = std::make_unique<llvm::Module>("name", context);
-    // llvm::FunctionType *funcType = llvm::FunctionType::get(llvm::Type::getInt32Ty(context), {llvm::Type::getInt32Ty(context)}, false);
-    // llvm::Function *func = llvm::Function::Create(funcType, llvm::Function::ExternalLinkage, "return_input", module.get());
-    // // create basic block and set insertion point
-    // llvm::BasicBlock *bb = llvm::BasicBlock::Create(context, "entry", func);
-    // llvm::IRBuilder<> builder(bb);
-    // // create function argument
-    // llvm::Value *arg = func->arg_begin();
-    // arg->setName("input");
-    // // return input argument
-    // builder.CreateRet(arg);
-    // // print LLVM IR code
-    // module->print(llvm::errs(), nullptr);
     generator::codeGenerator code_generator;
     program->accept(&code_generator);
     return 0;
